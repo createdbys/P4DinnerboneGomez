428ab2446a29566fadf905ac6610c1afb38bc4ee
MatthewPena1 <724514@student.dusd.net>
2023-05-30 01:44:06 -0700

    Worked on project
    Added obstacles in the field area
    Added a title screen scene that loads the main scene on button press

---
+++ p4DinnerboneGomezFinal/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour
+{
+
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
